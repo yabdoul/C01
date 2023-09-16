9949f34a9db6d2fad3d7ef8081ed216946b7a615
Youssef Abdoul <yabdoul@e2r3p2.1337.ma>
2023-09-16 21:17:34 +0100

    aa

--- ex08/sort.c
+++ ex08/sort.c
@@ -6,7 +6,7 @@
 /*   By: yabdoul <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 20:24:03 by yabdoul           #+#    #+#             */
-/*   Updated: 2023/09/16 21:16:15 by yabdoul          ###   ########.fr       */
+/*   Updated: 2023/09/16 21:14:32 by yabdoul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include<unistd.h>
@@ -19,13 +19,14 @@ void	ft_sort_int_tab(int *tab, int size)
 
 	i = 0;
 
-	while (i < size)
-	{
-		j= i + 1;
-		while (j < size-1)
+	while (i < size )
+	{ 
+	j=i+1;
+		while (j <size )
 		{
+			write(1,"c",1);
 			if (tab[i] > tab [j])
-			{
+			{ write(1,"a",1);
 				tmp = tab[i];
 				tab[i] = tab[j];
 				tab[j] = tmp;
@@ -34,4 +35,20 @@ void	ft_sort_int_tab(int *tab, int size)
 		}
 		i++;
 	}
+}
+
+
+int main ()
+{
+int tab [5] = {9,4,0,8,9};
+int i,a;
+i=0;
+ft_sort_int_tab(tab,5);
+while(i<5)
+{
+int a = tab[i]+'0';
+write(1,&a,1);
+i++ ;
+}
+return(0);
 }
