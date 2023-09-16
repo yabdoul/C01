e5640a86fdb16566f688754bb7c476b37eaa561f
Youssef Abdoul <yabdoul@e2r3p4.1337.ma>
2023-09-16 18:34:20 +0100

    aaa

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -1,18 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yabdoul <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/16 14:17:08 by yabdoul           #+#    #+#             */
-/*   Updated: 2023/09/16 14:47:19 by yabdoul          ###   ########.fr       */
+/*   Created: 2023/09/16 17:54:08 by yabdoul           #+#    #+#             */
+/*   Updated: 2023/09/16 18:33:26 by yabdoul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_ft(int *nbr)
+void	ft_rev_int_tab(int *tab, int size)
+
 {
-	*nbr = 42 ;
+	int	i;
+	int	tmp;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		tmp = tab[i];
+		tab[i] = tab[size - i];
+		tab[size - i] = tmp;
+		i++;
+	}
 }
